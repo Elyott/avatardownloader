a9433de6b630b1577b559c89b620fc64b93c4854
Elyott Ryan <elyott_ryan@hotmail.com>
2018-03-19 18:26:43 +0000

    added printchunks file that contains getAndPrintHTMLChunks function that receives then prints chunks of data from a https source

---
+++ printchunks.js
@@ -0,0 +1,26 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response){
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      console.log('Chunk received. Length: ', data.length,);
+      console.log(data, "\n")
+    });
+
+    response.on('end', function(){
+      console.log('Response Stream Finished');
+    });
+
+  });
+}
+
+console.log(getAndPrintHTMLChunks());
